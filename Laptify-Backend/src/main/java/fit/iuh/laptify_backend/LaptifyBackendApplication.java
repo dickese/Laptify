ddce4e3655c62a86f5e2aededca8f1c08d0ad0cd
hthinh24 <hungthinh240305@gmail.com>
2026-04-10 14:58:06 +0700

    setup backend

---
+++ Laptify-Backend/src/main/java/fit/iuh/laptify_backend/LaptifyBackendApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.laptify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptifyBackendApplication.class, args);
+	}
+
+}
---
+++ Laptify-Backend/src/test/java/fit/iuh/laptify_backend/LaptifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.iuh.laptify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
