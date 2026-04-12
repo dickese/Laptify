9becc85a6f94781d6e367e6825681aeb2daac858
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2026-04-12 10:31:33 +0700

    init FE vite project structure with react-router, tailwindcss

---
+++ Laptify-Frontend/src/pages/admin/product-page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductManagementPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductManagementPage
---
+++ Laptify-Frontend/src/pages/client/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+const RootPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <Header />
+      <div className='flex-grow'>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default RootPage
